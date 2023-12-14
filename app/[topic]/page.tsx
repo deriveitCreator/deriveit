a46326de0c7b0fafba8ac1f956bd1f74712d609d
Uzair <uzair.99@hotmail.com>
2023-12-14 02:26:54 -0500

    update 3.9

--- app/[topic]/page.tsx
+++ app/[topic]/page.tsx
@@ -13,11 +13,9 @@ type ImportType = {
 export default function Page({ params }: { params: { topic: string } }){
   useEffect(() => {window.scrollTo(0,0);});
   const designSelectedVal = parseInt(useCookies().get('designSelected')!) || DEFAULT_DESIGN_SELECTION;
-  const MainComp = dynamic<ImportType>(() => import(`./designs/Design${designSelectedVal}`));
   const decodedTopic = decodeURIComponent(params.topic);
   return (<>
     <TopicHeader text={decodedTopic} ds={designSelectedVal}/>
-    <MainComp topic={decodedTopic}/>
   </>)
 
 }
