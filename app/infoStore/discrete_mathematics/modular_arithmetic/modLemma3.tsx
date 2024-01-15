ecfac3fab031216860ab5a34582a8c3f05ffa061
Uzair <uzair.99@hotmail.com>
2024-01-14 21:28:34 -0500

    update 5.48

--- app/infoStore/discrete_mathematics/modular_arithmetic/modLemma3.tsx
+++ app/infoStore/discrete_mathematics/modular_arithmetic/modLemma3.tsx
@@ -1,3 +1,8 @@
 export const title="Proof that if ax ≡ bx (mod m) and gcd(x, m) = 1, then a ≡ b (mod m)";
-const content = [['h1', 'Proof that if ax ≡ bx (mod m) and gcd(x, m) = 1, then a ≡ b (mod m)'], ['pmain', 'If \\(ax ≡ bx \\bmod m\\):'], ['displayFormula', '$$\\begin{gathered} m \\mid (ax-bx) \\\\ m \\mid x(a-b) \\end{gathered}$$'], ['pmain', 'If \\(gcd(x, m) = 1\\):'], ['displayFormula', '$$\\begin{gathered}1 = j(x) + k(m) \\quad (where \\ j, k \\in &integers;)\\\\ (a-b) = j(a-b)(x) + k(a-b)(m) \\end{gathered}$$'], ['pmain', 'Since \\(m \\mid (a-b)(x)\\) and \\(m \\mid (a-b)(m)\\), then:'], ['displayFormula', '$$\\begin{gathered} m \\mid j(a-b)(x) + k(a-b)(m) \\\\ m \\mid (a-b) \\end{gathered}$$'], ['pmain', 'Therefore \\(a ≡ b \\bmod m\\).']];
+const content = [
+  ['h1', 'Proof that if ax ≡ bx (mod m) and gcd(x, m) = 1, then a ≡ b (mod m)'], ['pmain', 'If \\(ax ≡ bx \\bmod m\\):'],
+  ['displayFormula', '$$\\begin{gathered} m \\mid (ax-bx) \\\\ m \\mid x(a-b) \\end{gathered}$$'], ['pmain', 'If \\(gcd(x, m) = 1\\):'],
+  ['displayFormula', '$$1 = j(x) + k(m) \\quad (where \\ j, k \\in \\mathbb{Z}) $$$$ (a-b) = j(a-b)(x) + k(a-b)(m)$$'], ['pmain', 'Since \\(m \\mid (a-b)(x)\\) and \\(m \\mid (a-b)(m)\\), then:'],
+  ['displayFormula', '$$\\begin{gathered} m \\mid j(a-b)(x) + k(a-b)(m) \\\\ m \\mid (a-b) \\end{gathered}$$'], ['pmain', 'Therefore \\(a ≡ b \\bmod m\\).']
+];
 export default content;
