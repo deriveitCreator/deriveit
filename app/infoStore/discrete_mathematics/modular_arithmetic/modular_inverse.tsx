47d65767b8af6f2fccd8348f972bdc859d77decb
Uzair <uzair.99@hotmail.com>
2024-01-15 13:37:46 -0500

    update 5.61

--- app/infoStore/discrete_mathematics/modular_arithmetic/modular_inverse.tsx
+++ app/infoStore/discrete_mathematics/modular_arithmetic/modular_inverse.tsx
@@ -3,10 +3,10 @@ const content = [
   ['h1', 'Proving the existence of an inverse if (a, m) = 1'],
   ['pmain', 'If \\((a, m) = 1\\), then \\(ax+my=1\\) or \\(ax=m(q)+1\\), which means:'],
   ['displayFormula', '$$ ax ≡ 1 \\mod m $$'],
-  ['pmain', 'Here, \\(x\\) is the inverse. If \\(ab_1 ≡ 1 mod m\\) and \\(ab_2 ≡ 1 mod m\\), then:'],
-  ['displayFormula', '$$ ab_1 ≡ ab_2 mod m ⇒ m|a(b_1 - b_2) $$'],
+  ['pmain', 'Here, \\(x\\) is the inverse. If \\(ab_1 ≡ 1 \\mod m\\) and \\(ab_2 ≡ 1 \\mod m\\), then:'],
+  ['displayFormula', '$$ ab_1 ≡ ab_2 \\mod m ⇒ m|a(b_1 - b_2) $$'],
   ['pmain', 'Since \\((m, a) = 1\\):'],
-  ['displayFormula', '$$ m|b_1 - b_2 ⇒ b_1 ≡ b_2 mod m $$'],
+  ['displayFormula', '$$ m|b_1 - b_2 ⇒ b_1 ≡ b_2 \\mod m $$'],
   ['pmain', 'This shows that the inverse is unique.'],
 ];
 export default content;
