0a9ff9ed126de6ef40640f875f351d25b3f2667f
Uzair <uzair.99@hotmail.com>
2024-01-14 21:01:00 -0500

    update 5.46

--- app/infoStore/discrete_mathematics/divisibility_and_gcd/legendres_formula_lower_bound.tsx
+++ app/infoStore/discrete_mathematics/divisibility_and_gcd/legendres_formula_lower_bound.tsx
@@ -4,7 +4,7 @@ const content = [
   ['pmain', 'Given prime \\(p\\) and a positive integer \\(n\\), the Legendre\'s formula states:'],
   ['displayFormula','$$p^e \\| n! \\implies$$$$e = \\left \\lfloor \\frac{n}{p} \\right \\rfloor + \\left \\lfloor \\frac{n}{p^2} \\right \\rfloor + \\left \\lfloor \\frac{n}{p^3} \\right \\rfloor \\ldots$$'],
   ['pmain','This gives a lower bound:'],
-  ['displayFormula','$$e \\le \\frac{n}{p} + \\frac{n}{p} \\left ( \\frac{1}{p} \\right ) + \\frac{n}{p} \\left ( \\frac{1}{p^2} \\right ) + \\frac{n}{p} \\left ( \\frac{1}{p^3} \\right ) + \\ldots$$'],
+  ['displayFormula','$$e \\le \\frac{n}{p} + \\frac{n}{p} \\left ( \\frac{1}{p} \\right ) + \\frac{n}{p} \\left ( \\frac{1}{p} \\right )^2 + \\frac{n}{p} \\left ( \\frac{1}{p} \\right )^3 + \\ldots$$'],
   ['pmain','This is a geometric series, and using geometric series sum formula (<a href="https://www.deriveit.net/discrete_mathematics/sequence_and_series/the_sum_of_a_geometric_series_with_finite_terms">click here</a> for more info), we get:'],
   ['displayFormula','$$e \\le \\frac{ \\frac{n}{p} \\left ( \\frac{1}{p}^\\infty - 1 \\right ) }{\\left ( \\frac{1}{p} - 1 \\right ) }$$'],
   ['pmain','Since \\( \\frac{1}{p} \\) is less than one:'],
