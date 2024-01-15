cb079a673609a07bad3bcea78462726d3306f222
Uzair <uzair.99@hotmail.com>
2024-01-14 21:03:47 -0500

    update 5.47

--- app/infoStore/discrete_mathematics/divisibility_and_gcd/legendres_formula_lower_bound.tsx
+++ app/infoStore/discrete_mathematics/divisibility_and_gcd/legendres_formula_lower_bound.tsx
@@ -8,6 +8,8 @@ const content = [
   ['pmain','This is a geometric series, and using geometric series sum formula (<a href="https://www.deriveit.net/discrete_mathematics/sequence_and_series/the_sum_of_a_geometric_series_with_finite_terms">click here</a> for more info), we get:'],
   ['displayFormula','$$e \\le \\frac{ \\frac{n}{p} \\left ( \\frac{1}{p}^\\infty - 1 \\right ) }{\\left ( \\frac{1}{p} - 1 \\right ) }$$'],
   ['pmain','Since \\( \\frac{1}{p} \\) is less than one:'],
-  ['displayFormula','$$e \\le \\frac{ \\frac{1}{p} ( 0 - 1 ) }{\\frac{1}{p} - 1 }$$'],
+  ['displayFormula','$$e \\le \\frac{ \\frac{n}{p} ( 0 - 1 ) }{\\frac{1}{p} - 1 }$$'],
+  ['pmain','We can simplify this to:'],
+  ['displayFormula','$$e \\le \\frac{ n }{p - 1 }$$'],
 ];
 export default content;
