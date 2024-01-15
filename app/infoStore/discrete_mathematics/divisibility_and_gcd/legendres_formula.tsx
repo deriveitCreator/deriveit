90ae0adce47802388483ee83aa81be4eeab7ca0c
Uzair <uzair.99@hotmail.com>
2024-01-14 19:27:32 -0500

    update 5.39

--- app/infoStore/discrete_mathematics/divisibility_and_gcd/legendres_formula.tsx
+++ app/infoStore/discrete_mathematics/divisibility_and_gcd/legendres_formula.tsx
@@ -1,10 +1,10 @@
-export const title="Proving Legendre's Formula";
+export const title="Understanding Legendre's Formula";
 const content = [
-  ['h1', 'Proving Legendre\'s Formula'],
+  ['h1', 'Understanding Legendre\'s Formula'],
   ['pmain', 'Suppose we want to find how many times \\(3\\) divides \\(28!\\). Let\'s say by expanding \\(28!\\):'],
   ['displayFormula','$$(1)(2)(3)(4)(5)(6)(7)(8)(9)(10)(11)(12)(13)(14)(15)(16)(17)(18)(19)(20)(21)(22)(23)(24)(25)(26)(27)(28)$$'],
   ['pmain','There are in total \\(\\lfloor \\frac{n}{a} \\rfloor\\) multiples of \\(a\\) below \\(n\\), meaning there are  \\(\\lfloor \\frac{28}{3} \\rfloor\\) multiples of \\(3\\) below \\(28\\).'],
-  ['displayFormula','$$\\lfloor \\frac{28}{3} \\rfloor = 9$$'],
+  ['displayFormula','$$ \\left \\lfloor \\frac{28}{3} \\right \\rfloor = 9$$'],
   ['pmain','There are \\(9\\) numbers below \\(28\\) that are multiples of \\(3\\), which means there are \\(9\\) numbers in \\(28!\\) which can be divided by \\(3\\):'],
   ['displayFormula','$$ \\frac{(1)(2)(\\textcolor{red}{3})(4)(5)(\\textcolor{red}{6})(7)(8)(\\textcolor{red}{9})(10)(11)(\\textcolor{red}{12})(13)(14)(\\textcolor{red}{15})(16)(17)(\\textcolor{red}{18})(19)(20)(\\textcolor{red}{21})(22)(23)(\\textcolor{red}{24})(25)(26)(\\textcolor{red}{27})(28)}{3^9} $$$$ (1)(2)(\\textcolor{red}{1})(4)(5)(\\textcolor{red}{2})(7)(8)(\\textcolor{red}{3})(10)(11)(\\textcolor{red}{4})(13)(14)(\\textcolor{red}{5})(16)(17)(\\textcolor{red}{6})(19)(20)(\\textcolor{red}{7})(22)(23)(\\textcolor{red}{8})(25)(26)(\\textcolor{red}{9})(28) $$'],
   ['pmain','Looking at the numbers above, you can see that there are some numbers which can be divided by \\(3\\) again. There are in total \\(\\lfloor \\frac{28}{3^2} \\rfloor\\) (or \\(3\\)) numbers below \\(28\\) that can be divided by \\(3\\) twice:'],
@@ -12,6 +12,6 @@ const content = [
   ['pmain','Now there are in total \\(\\lfloor \\frac{28}{3^3} \\rfloor\\) (or \\(1\\)) number below \\(28\\) that can be divided by \\(3\\) a third time:'],
   ['displayFormula','$$ \\frac{(1)(2)(1)(4)(5)(2)(7)(8)(1)(10)(11)(4)(13)(14)(5)(16)(17)(2)(19)(20)(7)(22)(23)(8)(25)(26)(\\textcolor{red}{3})(28)}{3^1} $$$$ (1)(2)(1)(4)(5)(2)(7)(8)(1)(10)(11)(4)(13)(14)(5)(16)(17)(2)(19)(20)(7)(22)(23)(8)(25)(26)(\\textcolor{red}{1})(28) $$'],
   ['pmain','Since \\(\\lfloor \\frac{28}{3^4} \\rfloor = 0\\), then there are no numbers below \\(28\\) that can be divided by \\(3\\) \\(4\\) or more times. For the final number we calculated, if we take it\'s prime factorization, there would be no \\(3\\)\'s because we have divided it all. This means \\(3\\) can divide \\(28!\\) in total \\(\\lfloor \\frac{28}{3^1} \\rfloor + \\lfloor \\frac{28}{3^2} \\rfloor + \\lfloor \\frac{28}{3^3} \\rfloor = 13\\) times. Similarly, \\(5\\) can divide \\(28!\\) in total \\(\\lfloor \\frac{28}{5^1} \\rfloor + \\lfloor \\frac{28}{5^2} \\rfloor + \\lfloor \\frac{28}{5^3} \\rfloor \\ldots = 6\\) times. We can generalize this and say that prime \\(p\\) divides \\(n!\\) these many times:'],
-  ['displayFormula','$$\\lfloor \\frac{n}{p} \\rfloor + \\lfloor \\frac{n}{p^2} \\rfloor + \\lfloor \\frac{n}{p^3} \\ldots \\rfloor$$'],
+  ['displayFormula','$$\\left \\lfloor \\frac{n}{p} \\right \\rfloor + \\left \\lfloor \\frac{n}{p^2} \\right \\rfloor + \\left \\lfloor \\frac{n}{p^3} \\right \\rfloor \\ldots$$'],
 ];
 export default content;
