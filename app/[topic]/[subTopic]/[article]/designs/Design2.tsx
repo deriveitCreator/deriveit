1cf0226b807dd0044f4b213d8aff0045528e6ebb
Uzair <uzair.99@hotmail.com>
2024-01-21 19:25:13 -0500

    update 7.4

--- app/[topic]/[subTopic]/[article]/designs/Design2.tsx
+++ app/[topic]/[subTopic]/[article]/designs/Design2.tsx
@@ -137,7 +137,7 @@ export default function Design2(props: {topic: string, subTopic: string, article
     <section>
       <hr style={{backgroundColor:"black", height:"4px", border:"none", display:adHrDis}}/>
       {/*@ts-ignore*/}
-      <div align="center" ref={adRef}><ins class="adsbygoogle"
+      <div align="center" style={{overflowX:"scroll"}} ref={adRef}><ins class="adsbygoogle"
       style={{display:"block", textAlign:"center", maxWidth:"95%", marginTop:"20px", marginBottom:"20px"}}
       data-ad-layout="in-article"
       data-ad-format="fluid"
